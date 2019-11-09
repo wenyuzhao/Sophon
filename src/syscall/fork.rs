64b175be60840792e1a576d4ecdfc43799b0cd8f
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2019-11-09 14:34:39 +1100

    Minor fix

--- src/syscall/fork.rs
+++ src/syscall/fork.rs
@@ -1,6 +1,6 @@
 use crate::exception::ExceptionFrame;
 
-pub fn fork(exception_frame: *mut ExceptionFrame) -> isize {
+pub fn fork(exception_frame: &mut ExceptionFrame) -> isize {
     let parent_task = crate::task::Task::current().unwrap();
     let child_task = parent_task.fork(exception_frame as *const ExceptionFrame as usize);
     return unsafe { ::core::mem::transmute(child_task.id()) }
--- src/syscall/mod.rs
+++ src/syscall/mod.rs
@@ -5,32 +5,27 @@ pub mod fork;
 use crate::exception::ExceptionFrame;
 
 #[repr(usize)]
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
 pub enum SysCall {
     Fork = 0x0,
-    __MAX_SYSCALLS,
+    #[allow(non_camel_case_types)] __MAX_SYSCALLS,
 }
 
-type SysCallHandler = fn (exception_frame: *mut ExceptionFrame) -> isize;
+type SysCallHandler = fn (exception_frame: &mut ExceptionFrame) -> isize;
 
 static SYSCALL_HANDLERS: [SysCallHandler; SysCall::__MAX_SYSCALLS as usize] = [
     fork::fork,
 ];
 
-#[no_mangle]
-pub extern fn handle_syscall(exception_frame: *mut ExceptionFrame) {
+
+pub unsafe fn handle_syscall(exception_frame: &mut ExceptionFrame) {
     debug!("exception_frame@{:?}", exception_frame as *mut _);
     let syscall_id: SysCall = unsafe { ::core::mem::transmute((*exception_frame).x0) };
     debug!("Syscall: {:?}", syscall_id);
-    match syscall_id {
-        SysCall::Fork => {
-            // let parent_task = crate::task::Task::current().unwrap();
-            // let child_task = parent_task.fork(exception_frame as *mut ExceptionFrame as usize);
-            // unsafe { asm!("DSB SY") };
-            unsafe { (*exception_frame).x0 = ::core::mem::transmute(fork::fork(exception_frame)) };
-        }
-        _ => unreachable!()
-    }
+    let handler = SYSCALL_HANDLERS[syscall_id as usize];
+    let result = handler(exception_frame);
+    debug!("Syscall {:?} returned {:?}", syscall_id, result);
+    exception_frame.x0 = ::core::mem::transmute(fork::fork(exception_frame));
 }
 
 
