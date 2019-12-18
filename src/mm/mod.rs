528e57fd004814caad9fc9ebca2e59123b3f3a20
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2019-12-18 22:40:59 +0800

    Remove redundant files

--- src/mm/mod.rs
+++ src/mm/mod.rs
@@ -1,15 +1,11 @@
 pub mod address;
 pub mod heap_constants;
 pub mod page;
-// pub mod frame_allocator;
-// pub mod page_table;
-// pub mod paging;
 pub mod heap;
 
 pub use self::address::*;
 pub use self::page::*;
-// pub use self::page_table::PageFlags;
-// pub use self::paging::*;
+
 use crate::arch::*;
 use Target::MemoryManager;
 
@@ -45,65 +41,3 @@ pub fn memory_map(address: Address, size: usize, mut flags: PageFlags) -> Result
     }
     Ok(address)
 }
-
-// pub fn map_user<S: PageSize>(page: Page<S>, frame: Frame<S>, mut flags: PageFlags) -> Page<S> {
-//     MemoryManager::map(page, frame, flags);
-//     page
-// }
-
-// pub fn map_kernel<S: PageSize>(page: Page<S>, frame: Frame<S>, mut flags: PageFlags) {
-//     MemoryManager::map(page, frame, flags);
-// }
-
-// pub fn update_kernel_page_flags<S: PageSize>(page: Page<S>, mut flags: PageFlags) {
-//     MemoryManager::update_flags(page, flags);
-// }
-
-// Unmap a kernel page, optionally release its corresponding frame
-// pub fn unmap_kernel<S: PageSize>(page: Page<S>, release_frame: bool) {
-//     let (frame, _) = MemoryManager::translate(page).unwrap();
-//     MemoryManager::unmap(page);
-//     if release_frame {
-//         MemoryManager::dealloc_frame(frame);
-//     }
-// }
-
-
-
-// use core::ops::*;
-
-// pub struct TemporaryKernelPage<S: PageSize>(Page<S>, bool);
-
-// impl <S: PageSize> Deref for TemporaryKernelPage<S> {
-//     type Target = Page<S>;
-//     fn deref(&self) -> &Self::Target {
-//         &self.0
-//     }
-// }
-
-// impl <S: PageSize> Drop for TemporaryKernelPage<S> {
-//     fn drop(&mut self) {
-//         unmap_kernel(self.0, self.1);
-//         paging::invalidate_tlb();
-//     }
-// }
-
-
-// pub fn map_kernel_temporarily<S: PageSize>(frame: Frame<S>, mut flags: PageFlags, p: Option<usize>) -> TemporaryKernelPage<S> {
-//     const MAGIC_PAGE: usize = 0xffff_1234_5600_0000;
-//     let page = Page::new(p.unwrap_or(MAGIC_PAGE).into());
-//     map_kernel(page, frame, flags);
-//     paging::invalidate_tlb();
-//     TemporaryKernelPage(page, false)
-// }
-
-// pub fn handle_user_pagefault(address: Address) {
-//     // let p4 = PageTable::<L4>::get(false);
-//     // if let Some((_, flags)) = p4.translate(address) {
-//     //     if flags.contains(PageFlags::COPY_ON_WRITE) {
-//     //         p4.fix_copy_on_write(address, !flags.contains(PageFlags::SMALL_PAGE));
-//     //         return
-//     //     }
-//     // }
-//     unimplemented!()
-// }
