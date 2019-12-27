ceab5054c939670ffa7bec4272215233b5871aa6
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2019-12-27 17:11:46 +0800

    Update build scripts

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 arch ?= aarch64
 
+profile = $(if $(release),release,debug)
+cargo_xbuild = cargo xbuild --no-default-features  $(if $(release), --release)
+
 include Makefile.$(arch).mk
 
 FORCE: ;
--- Makefile.aarch64.mk
+++ Makefile.aarch64.mk
@@ -10,7 +10,6 @@ device ?= raspi3-qemu
 # Optional: dint=1
 
 # Derived configurations
-profile = $(if $(release),release,debug)
 debug_interrupts = $(if $(dint),-d int)
 output_elf = target/$(kernel_target)/$(profile)/proton
 output_img = target/$(kernel_target)/$(profile)/kernel8.img
@@ -22,28 +21,24 @@ qemu_gdb_server = $(if $(gdb),-s -S)
 
 
 kernel: init FORCE
-	@cd kernel && cargo xbuild --no-default-features $(if $(release), --release) --target $(kernel_target).json --features device-$(device)
+	@cd kernel && $(cargo_xbuild) --target $(kernel_target).json --features device-$(device)
 	@llvm-objcopy --strip-all $(output_elf) -O binary $(output_img)
+	@llvm-objdump --source -d $(output_elf) > $(output_elf).s
 
 init: FORCE
-	@cd init && cargo xbuild --no-default-features $(if $(release), --release) --target $(user_target).json
+	@cd init && $(cargo_xbuild) --target $(user_target).json
 	@cp target/$(user_target)/$(profile)/init target/init
+	@llvm-objdump --source -d $(output_init_elf) > $(output_init_elf).s
 
 run: device=raspi3-qemu
-run: kernel objdump
+run: kernel
 	@$(qemu_command) $(qemu_debug_interrupts) $(qemu_gdb_server) -kernel $(output_img)
-
-objdump:
-	@llvm-objdump --source -d $(output_elf) > kernel.S
-	@llvm-objdump --source -d $(output_init_elf) > init.S
 
 gdb:
 	@gdb-multiarch -quiet "$(output_elf)" -ex "set arch aarch64" -ex "target remote :1234"
 
 clean:
 	@cargo clean
-	@cd init && cargo clean
-	@rm ./Cargo.lock ./kernel.S ./init.S ./kernel8.img
 
 copy-win:
 	@PowerShell.exe -Command "copy $(output_img) D:/"
