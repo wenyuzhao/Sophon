1b51ad2cbb4e7dcf4bb4e6d1422a6ac205504f20
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2021-07-12 13:55:52 +1000

    cleanup

--- init/src/main.rs
+++ init/src/main.rs
@@ -14,49 +14,8 @@ static ALLOCATOR: NoAlloc = NoAlloc;
 
 #[no_mangle]
 pub extern "C" fn _start(_argc: isize, _argv: *const *const u8) -> isize {
-    // unsafe { llvm_asm!("1:  b 1b") }
     log!("Init process start (user mode)");
-
-    // let msg = Message {
-    //     sender: 0,
-    //     receiver: 0,
-    //     kind: 233,
-    //     data: [0; 16]
-    // };
-
-    // msg.send();
-
-    // loop {
-    //     let mut m = Message::receive(None);
-    //     if m.kind == 233 {
-    //         log!("Init received #233: {:?}", m.data[0]);
-    //         m.data[0] += 1;
-    //         m.receiver = m.sender;
-    //         m.send()
-    //     }
-    // }
-
-    // let id = KernelCall::fork().unwrap();
-    // log!("Fork return -> {:?}", id);
     loop {}
-    // unreachable!();
-    // let id = syscall!(SysCall::Fork);
-    // log!("Hello from init process! <{}>", id);
-    // // loop {}
-    // for i in 0..100 {
-    //     log!("Hello from init process! <{}>", id);
-    // }
-
-    // if id == 0 {
-    //     log!("Child process exit...");
-    //     syscall!(SysCall::Exit, 0usize);
-    // }
-    // loop {
-    //     log!("Hello from init process! <{}>", id);
-    //     for i in 0..100000 {
-    //         // unsafe { llvm_asm!("nop") }
-    //     }
-    // }
 }
 
 #[panic_handler]
