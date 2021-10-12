0b7e85a0f404ec02116d2c4131227b7e31d6f22e
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2021-10-13 10:18:52 +1100

    Fix locks in interrupt handler code

--- sophon/src/schemes/mod.rs
+++ sophon/src/schemes/mod.rs
@@ -5,9 +5,9 @@ mod user;
 use crate::task::{Proc, Task};
 use alloc::{
     borrow::ToOwned,
-    boxed::Box,
     collections::BTreeMap,
     string::{String, ToString},
+    sync::Arc,
 };
 use core::{
     intrinsics::transmute,
@@ -31,28 +31,32 @@ trait SchemeIdExt: Sized {
 impl SchemeIdExt for SchemeId {}
 
 pub static SCHEME_IDS: Mutex<BTreeMap<String, SchemeId>> = Mutex::new(BTreeMap::new());
-pub static SCHEMES: Mutex<BTreeMap<SchemeId, Box<dyn SchemeServer + Send>>> =
+pub static SCHEMES: Mutex<BTreeMap<SchemeId, Arc<dyn SchemeServer + Send + Sync>>> =
     Mutex::new(BTreeMap::new());
 
-fn register_kernel_scheme(scheme: Box<dyn SchemeServer + Send>) {
+fn register_kernel_scheme(scheme: Arc<dyn SchemeServer + Send + Sync>) {
     let id = SchemeId::alloc();
     SCHEME_IDS.lock().insert(scheme.name().to_owned(), id);
     SCHEMES.lock().insert(id, scheme);
 }
 
 pub fn register_kernel_schemes() {
-    register_kernel_scheme(box system::SystemScheme::new());
-    register_kernel_scheme(box initfs::InitFSScheme::new());
+    register_kernel_scheme(Arc::new(system::SystemScheme::new()));
+    register_kernel_scheme(Arc::new(initfs::InitFSScheme::new()));
+}
+
+fn get_scheme_by_id(id: SchemeId) -> Arc<dyn SchemeServer> {
+    SCHEMES.lock().get(&id).unwrap().clone()
 }
 
 pub fn handle_scheme_request(args: &[usize; 5]) -> Result<isize, isize> {
     match unsafe { transmute::<_, SchemeRequest>(args[0]) } {
         SchemeRequest::Register => {
             let name = unsafe { transmute::<_, &&str>(args[1]) };
-            register_kernel_scheme(box user::UserScheme::new(
+            register_kernel_scheme(Arc::new(user::UserScheme::new(
                 name.to_string(),
                 Task::current().id,
-            ));
+            )));
             Ok(0)
         }
         SchemeRequest::Open => {
@@ -63,8 +67,7 @@ pub fn handle_scheme_request(args: &[usize; 5]) -> Result<isize, isize> {
                 Uri::new(uri_str).unwrap()
             };
             let scheme_id = SchemeId::from_name(uri.scheme).unwrap();
-            let schemes = SCHEMES.lock();
-            let scheme = schemes.get(&scheme_id).unwrap();
+            let scheme = get_scheme_by_id(scheme_id);
             let resource = scheme
                 .open(&uri, args[3] as _, unsafe { transmute(args[4]) })
                 .unwrap();
@@ -87,8 +90,7 @@ pub fn handle_scheme_request(args: &[usize; 5]) -> Result<isize, isize> {
                 let buf_len = transmute::<_, usize>(args[3]);
                 slice::from_raw_parts_mut(buf_ptr, buf_len)
             };
-            let schemes = SCHEMES.lock();
-            let scheme = schemes.get(&Proc::current().resources.lock()[&fd]).unwrap();
+            let scheme = get_scheme_by_id(Proc::current().resources.lock()[&fd]);
             let r = scheme.read(fd, buf).unwrap();
             Ok(unsafe { transmute(r) })
         }
@@ -99,8 +101,7 @@ pub fn handle_scheme_request(args: &[usize; 5]) -> Result<isize, isize> {
                 let buf_len = transmute::<_, usize>(args[3]);
                 slice::from_raw_parts(buf_ptr, buf_len)
             };
-            let schemes = SCHEMES.lock();
-            let scheme = schemes.get(&Proc::current().resources.lock()[&fd]).unwrap();
+            let scheme = get_scheme_by_id(Proc::current().resources.lock()[&fd]);
             scheme.write(fd, buf).unwrap();
             Ok(0)
         }
